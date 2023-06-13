a55b03db6282d70821718864ca7e3b4948cc2fb6
MarcinBieleniewicz <73874499+MarcinBieleniewicz@users.noreply.github.com>
2023-06-13 23:55:59 +0200

    Add files via upload

---
+++ mongoexport.py
@@ -0,0 +1,10 @@
+import pymongo
+import json
+
+client = pymongo.MongoClient('mongosh mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000')
+db = client['skany']
+collection = db['skany_pozycji']
+document = list(collection.find())
+
+with open('baza_pozycji.json', 'w') as file:
+    json.dump(document, file)
