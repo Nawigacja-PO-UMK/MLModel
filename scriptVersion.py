222c8a879562dd21815141f603fa4d568f4da8f5
MarcinBieleniewicz <73874499+MarcinBieleniewicz@users.noreply.github.com>
2023-06-18 23:26:40 +0200

    Update scriptVersion.py

--- scriptVersion.py
+++ scriptVersion.py
@@ -69,7 +69,7 @@ def trainModel():
     X = scaler.fit_transform(X)
 
 
-    model = RandomForestRegressor(n_estimators=100, random_state=42)
+    model = RandomForestRegressor(n_estimators=200, max_depth=10, min_samples_split=2, min_samples_leaf=1, max_features="auto", random_state=42)
 
     model.fit(X, y)
 
